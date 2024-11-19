cda244ffda6ea25bfabcc41c8d360f669d04127c
Chaitanya <chaitu.raju@gmail.com>
2024-11-19 11:40:25 +0530

    added MuI theme

--- qtify/src/index.js
+++ qtify/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, Arial, sans-serif", // Apply your desired font family
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
